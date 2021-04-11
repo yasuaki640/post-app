102fa75efa097fcafbe9455622c90dfd34716c94
yasuaki640 <yasuaki640@gmail.com>
2021-04-11 15:41:54 +0900

    fix validation

--- sampleapp/app/Http/Requests/UserRegistRequest.php
+++ sampleapp/app/Http/Requests/UserRegistRequest.php
@@ -26,7 +26,7 @@ class UserRegistRequest extends FormRequest
     {
         return [
             'name' => 'required|string',
-            'email' => 'required|string',
+            'email' => 'required|email',
             'password' => 'required|string',
         ];
     }
