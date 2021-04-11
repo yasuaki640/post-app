c93238d960a1d11e46036781dc203e9d1ff5b12a
yasuaki640 <yasuaki640@gmail.com>
2021-04-11 19:08:32 +0900

    route create post action

--- sampleapp/routes/api.php
+++ sampleapp/routes/api.php
@@ -18,4 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/user','App\Http\Controllers\UserController@register');
+Route::post('/user', 'App\Http\Controllers\UserController@register');
+
+Route::post('/post', 'App\Http\Controllers\PostController@post');
