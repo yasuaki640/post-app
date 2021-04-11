8718f4f2757f32f005265e7f4ed120022b45de58
yasuaki640 <yasuaki640@gmail.com>
2021-04-11 15:42:13 +0900

    user repo

---
+++ sampleapp/app/Repository/UserRepository.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repository;
+
+
+use App\Models\User;
+
+class UserRepository
+{
+    public function create(string $name, string $email, string $password): int
+    {
+        return User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+        ])->id;
+    }
+}
