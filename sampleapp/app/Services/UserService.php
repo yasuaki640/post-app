098d396ff9ce06f314d7b62549fc02577b0d5f46
yasuaki640 <yasuaki640@gmail.com>
2021-04-11 15:42:08 +0900

    user service

--- sampleapp/app/Services/UserService.php
+++ sampleapp/app/Services/UserService.php
@@ -1,10 +1,24 @@
 <?php
-
+declare(strict_types=1);
 
 namespace App\Services;
 
 
+use App\Repository\UserRepository;
+use JetBrains\PhpStorm\Pure;
+
 class UserService
 {
+    private UserRepository $repo;
 
+    #[Pure] public function __construct()
+    {
+        $this->repo = new UserRepository;
+    }
+
+    public function register(string $name, string $email, string $password): int
+    {
+        $id = $this->repo->create($name, $email, $password);
+        return $id;
+    }
 }
