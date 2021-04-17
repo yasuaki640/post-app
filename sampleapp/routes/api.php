742e37102831ad1f6d346ba564dea5c875efb84f
yasuaki640 <yasuaki640@gmail.com>
2021-04-17 20:50:13 +0900

    delete routings

--- sampleapp/routes/api.php
+++ sampleapp/routes/api.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,18 +11,4 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-$api = app(Router::class);
 
-$api->group(['middleware'=>'jwt.auth'],function ());
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::post('/user', [UserController::class, 'register']);
-Route::get('/user/{id}', [UserController::class, 'getById']);
-
-Route::post('/post', [PostController::class, 'post']);
-Route::get('/post', [PostController::class, 'list']);
-
-Route::post('/login', [AuthController::class, 'login']);
