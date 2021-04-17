dff55a4737054f9bd3c5456806c08ac9fe0eefa7
yasuaki640 <yasuaki640@gmail.com>
2021-04-17 21:07:27 +0900

    sikirinaosi

--- sampleapp/routes/api.php
+++ sampleapp/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,3 +14,8 @@
 |
 */
 
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('users',\App\Http\Controllers\UserController::class);
---
+++ sampleapp/tests/Feature/Controllers/UserControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    public function test_create_validation_failer()
+    {
+        $response = $this->post('/api/users', []);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
