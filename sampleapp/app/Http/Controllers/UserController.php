6d512ce40eab760d959caa49b6cd502b5162ed57
yasuaki640 <yasuaki640@gmail.com>
2021-04-20 22:53:06 +0900

    tukawannnodekesu

--- sampleapp/app/Http/Controllers/UserController.php
+++ sampleapp/app/Http/Controllers/UserController.php
@@ -34,16 +34,6 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param StoreRequest $request
