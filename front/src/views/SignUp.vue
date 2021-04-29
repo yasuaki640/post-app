89197e4422cacf479d08e82dad2a381caadbf34a
yasuaki640 <yasuaki640@gmail.com>
2021-04-29 18:11:38 +0900

    ;liszjdshsgojaeirzuj

--- front/src/views/SignUp.vue
+++ front/src/views/SignUp.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>Enter your account info</h1>
+    <form>
+      <ul>
+        <li>
+          <label for="name">name :
+            <input id="name" type="text">
+          </label>
+        </li>
+        <li>
+          <label for="email">password :
+            <input id="email" type="email">
+          </label>
+        </li>
+      </ul>
+    </form>
   </div>
 </template>
