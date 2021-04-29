475c25ee5ac1a4857ac6f659f5a77ae03a0a286e
yasuaki640 <yasuaki640@gmail.com>
2021-04-29 17:35:23 +0900

    Revert "殺伐とした開発環境に突然の"
    This reverts commit d7bd9790a155901d7b0e85bbbe5a7708fa7c97ff.

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <Index msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Index from './components/Index'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Index
+    HelloWorld
   }
 }
 </script>
