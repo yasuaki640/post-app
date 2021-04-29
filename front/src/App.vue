d7bd9790a155901d7b0e85bbbe5a7708fa7c97ff
yasuaki640 <yasuaki640@gmail.com>
2021-04-29 17:30:26 +0900

    殺伐とした開発環境に突然の

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Index msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/Index'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
---
+++ front/src/components/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>✝✝✝ 殺伐とした開発環境に突然の投稿アプリ ✝✝✝</h1>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
