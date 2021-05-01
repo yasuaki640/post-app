30b1b856a1aa54138a247a6233f4d045f9eb6894
yasuaki640 <yasuaki640@gmail.com>
2021-05-01 22:42:34 +0900

    あおい；ｊｇｋんゔぇぞ

--- front/src/main.js
+++ front/src/main.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import router from './router'
 import VueAxios from "vue-axios";
 
+require('@/assets/scss/style.scss')
+
 const app = createApp(App)
 app.use(router)
 app.use(VueAxios, axios)
