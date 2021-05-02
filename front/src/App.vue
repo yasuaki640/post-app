22bcd26bf00de6e5d015b0dbd8cc41e676ee7b1f
yasuaki640 <yasuaki640@gmail.com>
2021-05-02 23:24:09 +0900

    ままま

--- front/src/App.vue
+++ front/src/App.vue
@@ -8,15 +8,16 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
 }
 
 #nav {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   padding: 30px;
+  text-align: center;
+  color: #2c3e50;
 }
 
 #nav a {
