4f274f0ae6a9565e66793a7719d4390037a4aa06
yasuaki640 <yasuaki640@gmail.com>
2021-05-05 21:14:02 +0900

    ;kljijo

--- front/src/main.js
+++ front/src/main.js
@@ -1,16 +1,19 @@
+import './bootstrap/axios'
+import './assets/scss/style.scss'
 import Vue from 'vue'
 import App from './App.vue'
 import axios from "axios";
 import router from './router'
-import store from "@/store";
+import store from "./store";
 import VueAxios from "vue-axios";
 
-const createApp = async ()=>{
+const createApp = async () => {
 
     await store.dispatch('auth/loginUser')
+
     Vue.config.productionTip = false
+
     Vue.use(VueAxios, axios)
-    require('@/assets/scss/style.scss')
 
     new Vue({
         router,
