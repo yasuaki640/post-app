391981e0342300bbc08d0a59931d909b3afefe17
yasuaki640 <yasuaki640@gmail.com>
2021-05-05 18:54:50 +0900

    本番はだめだけど今回はjwtで行きます

---
+++ front/src/bootstrap/axios.js
@@ -0,0 +1,14 @@
+window.axios = require('axios')
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+window.axios.interceptors.request.use(config => {
+    config.headers['Authorization'] = localStorage.getItem('post_app_token')
+
+    return config
+})
+
+window.axios.interceptors.response.use(
+    response => response,
+    error => error.response || error
+)
