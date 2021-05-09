415e9af2731cc52799698613eeea1cf1b5e6b614
yasuaki640 <yasuaki640@gmail.com>
2021-05-09 09:39:21 +0900

    ああなんか疲れたな

--- front/src/components/Post.vue
+++ front/src/components/Post.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="post">
     <hr>
-    <p>id :{{ post.id }} user_id:{{ post.user_id }} sentAt:{{post.created_at}}</p>
+    <p>id :{{ post.id }} user_id:{{ post.user_id }} sentAt:{{ changeDisplayTimeBySentAt(post.created_at) }}</p>
     <p>{{ post.body }}</p>
   </div>
 </template>
@@ -11,8 +11,48 @@
 export default {
   props: {
     post: {
-      type: Object,
-      required: true
+      id: {
+        type: Number,
+        required: true
+      },
+      user_id: {
+        type: Number,
+        required: true
+      },
+      body: {
+        type: String,
+        required: true
+      },
+      created_at: {
+        type: String,
+        required: true
+      }
+    }
+  },
+  methods: {
+    // if sent at is today return hh:mm time str、else mm/dd time str。
+    changeDisplayTimeBySentAt(sentAtStr) {
+      if (!sentAtStr) {
+        return
+      }
+
+      const sentAtDate = new Date(sentAtStr)
+
+      if (this.isToday(sentAtDate)) {
+        const h = sentAtDate.getHours().toString().padStart(2, '0')
+        const m = sentAtDate.getMinutes().toString().padStart(2, '0')
+        return `${h}:${m}`
+
+      } else {
+        const m = (sentAtDate.getMonth() + 1).toString().padStart(2, '0')
+        const d = sentAtDate.getDate().toString().padStart(2, '0')
+        return `${m}/${d}`
+      }
+    },
+
+    isToday(date) {
+      const today = new Date();
+      return date.toDateString() === today.toDateString();
     }
   }
 }
